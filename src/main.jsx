14f335fee8929ca7b0acbf7e17d67ad4605c3752
pawankushwah <pawankushwahmail@gmail.com>
2026-05-05 12:31:03 +0530

    updated navigation config for single page application

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext'
-import { BrowserRouter, Routes, Route, useParams, useNavigate } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useParams, useNavigate, Navigate } from 'react-router-dom'
 
 const AppWrapper = () => {
   const param = useParams();
@@ -17,7 +17,9 @@ createRoot(document.getElementById('root')).render(
     <AuthProvider>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<App tab="home" />} />
           <Route path="/:tab" element={<AppWrapper />} />
+
         </Routes>
       </BrowserRouter>
     </AuthProvider>
